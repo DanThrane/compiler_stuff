ae6ec552b4a5d3da014ba9c41af41a0b19c1f943
Dan Sebastian Thrane <dthrane@gmail.com>
2015-11-16 20:44:40 +0100

    Parameter declarations

--- src/main/kotlin/dk/thrane/compiler/ast/Parser.kt
+++ src/main/kotlin/dk/thrane/compiler/ast/Parser.kt
@@ -4,7 +4,7 @@ import dk.thrane.compiler.ast.Tokens.*
 
 class Parser() {
     fun parse() {
-        val source = "func                 thisIsAFuncIdentifier (      ) end thisIsAFuncIdentifier"
+        val source = "func                 thisIsAFuncIdentifier (  test: int, test2: array of record of {test2: array of int}    ) end thisIsAFuncIdentifier"
         var cursor = Cursor(source)
 
         function(cursor)
@@ -20,13 +20,53 @@ class Parser() {
         Tokens.consume(T_FUNC, cursor)
         var id = Tokens.consume(T_ID, cursor)
         Tokens.consume(T_LPAR, cursor)
+        variableDeclarationList(cursor)
         Tokens.consume(T_RPAR, cursor)
 
         println(id.image)
     }
 
     private fun functionBody(cursor: Cursor) {
+        var next = Tokens.nextToken(cursor, false)
+        when (next.tokenType) {
+            T_ID -> variableDeclarationList(cursor)
+            T_END -> return
+            else -> throw IllegalStateException("Unexpected token expected '${next.tokenType}', expected $T_ID or " +
+                    "$T_END")
+        }
+    }
 
+    private fun variableDeclarationList(cursor: Cursor) {
+        var next = Tokens.nextToken(cursor, false)
+        when (next.tokenType) {
+            T_ID -> {
+                var id = Tokens.consume(T_ID, cursor)
+                Tokens.consume(T_COLON, cursor)
+                type(cursor)
+                Tokens.optionallyConsume(T_COMMA, cursor)
+                variableDeclarationList(cursor)
+            }
+            else -> {}
+        }
+    }
+
+    private fun type(cursor: Cursor): Any {
+        var next = Tokens.nextToken(cursor)
+        when (next.tokenType) {
+            T_ID, T_INT, T_BOOL, T_CHAR -> return next.tokenType
+            T_ARRAY -> {
+                type(cursor)
+                return next.tokenType
+            }
+            T_RECORD -> {
+                Tokens.consume(T_LBRACE, cursor)
+                variableDeclarationList(cursor)
+                Tokens.consume(T_RBRACE, cursor)
+                return next.tokenType
+            }
+            else -> throw IllegalStateException("Unexpected token '${next.tokenType}'. Expected a type here. " +
+                    "At ${cursor.remainingString}")
+        }
     }
 
     private fun functionTail(cursor: Cursor) {
--- src/main/kotlin/dk/thrane/compiler/ast/Tokens.kt
+++ src/main/kotlin/dk/thrane/compiler/ast/Tokens.kt
@@ -3,53 +3,53 @@ package dk.thrane.compiler.ast
 import java.util.regex.Pattern
 
 enum class Tokens {
-    T_MULT,
-    T_DIV,
-    T_ADD,
-    T_SUB,
+    T_MULT("\\*"),
+    T_DIV("/"),
+    T_ADD("\\+"),
+    T_SUB("-"),
     T_LPAR("\\("),
     T_RPAR("\\)"),
-    T_EQUAL,
-    T_CEQ,
-    T_CNE,
-    T_CLE,
-    T_CRE,
-    T_LEQ,
-    T_GEQ,
-    T_LBRACE,
-    T_RBRACE,
-    T_PER,
-    T_BANG,
-    T_OR,
-    T_VBAR,
-    T_COLON,
-    T_SCOLON,
-    T_DOT,
-    T_COMMA,
-    T_LSBRACE,
-    T_RSBRACE,
-    T_AND,
+    T_EQUAL("="),
+    T_CEQ("=="),
+    T_CNE("!="),
+    T_CLE("<"),
+    T_CRE(">"),
+    T_LEQ("<="),
+    T_GEQ(">="),
+    T_LBRACE("\\{"),
+    T_RBRACE("\\}"),
+    T_PER("%"),
+    T_BANG("!"),
+    T_OR("||"),
+    T_VBAR("|"),
+    T_COLON(":"),
+    T_SCOLON(";"),
+    T_DOT("\\."),
+    T_COMMA(","),
+    T_LSBRACE("\\["),
+    T_RSBRACE("\\]"),
+    T_AND("&&"),
     T_FUNC("func"),
-    T_INT,
-    T_CHAR,
-    T_BOOL,
-    T_ARRAY,
-    T_RECORD,
+    T_INT("int"),
+    T_CHAR("char"),
+    T_BOOL("bool"),
+    T_ARRAY("array of"),
+    T_RECORD("record of"),
     T_END("end"),
-    T_VAR,
-    T_RETURN,
-    T_WRITE,
-    T_ALLOC,
-    T_IF,
-    T_THEN,
-    T_WHILE,
-    T_DO,
-    T_LENGTH,
-    T_ELSE,
-    T_TRUE,
-    T_FALSE,
-    T_NULL,
-    T_TYPE,
+    T_VAR("var"),
+    T_RETURN("return"),
+    T_WRITE("write"),
+    T_ALLOC("alloc"),
+    T_IF("if"),
+    T_THEN("then"),
+    T_WHILE("while"),
+    T_DO("do"),
+    T_LENGTH("of length"),
+    T_ELSE("else"),
+    T_TRUE("true"),
+    T_FALSE("false"),
+    T_NULL("null"),
+    T_TYPE("type"),
     T_ID("[a-zA-Z_][a-zA-Z0-9_]*"),
     T_WHITESPACE(" +");
 
@@ -63,19 +63,25 @@ enum class Tokens {
     }
 
     companion object {
-        fun consume(type: Tokens, cursor: Cursor): Token {
+        fun optionallyConsume(type: Tokens, cursor: Cursor): Token? {
             T_WHITESPACE.consume(cursor)
-            return type.consume(cursor) ?: throw IllegalStateException("Syntax error at '${cursor.remainingString.substring(0, 10)}' expected token ${type}!")
+            return type.consume(cursor)
         }
 
-        fun nextToken(cursor: Cursor): Token? {
+        fun consume(type: Tokens, cursor: Cursor): Token {
+            return optionallyConsume(type, cursor) ?: throw IllegalStateException("Syntax error at " +
+                    "'${cursor.remainingString}' expected token $type!")
+        }
+
+        fun nextToken(cursor: Cursor, eat: Boolean = true): Token {
+            T_WHITESPACE.consume(cursor)
             for (type in values) {
-                var token = type.consume(cursor)
+                var token = type.consume(cursor, eat)
                 if (token != null) {
                     return token
                 }
             }
-            return null
+            throw IllegalStateException("Unexpected end of file at ${cursor.remainingString}")
         }
 
         private fun consumeRegex(cursor: Cursor, regex: Pattern): String? {
@@ -94,10 +100,12 @@ enum class Tokens {
             return match
         }
 
-        private fun consumeRegexToken(type: Tokens, cursor: Cursor, pattern: Pattern): Token? {
+        private fun consumeRegexToken(type: Tokens, cursor: Cursor, pattern: Pattern, eat: Boolean = true): Token? {
             var token = consumeRegex(cursor, pattern)
             if (token != null) {
-                cursor.advance(token.length)
+                if (eat) {
+                    cursor.advance(token.length)
+                }
                 return Token(type, token)
             } else {
                 return null
@@ -105,9 +113,9 @@ enum class Tokens {
         }
     }
 
-    private fun consume(cursor: Cursor): Token? {
+    private fun consume(cursor: Cursor, eat: Boolean = true): Token? {
         if (regex != null) {
-            return consumeRegexToken(this, cursor, regex as Pattern)
+            return consumeRegexToken(this, cursor, regex as Pattern, eat)
         }
         return null
     }
